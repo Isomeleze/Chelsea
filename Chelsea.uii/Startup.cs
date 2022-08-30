3ba07385b2d0a4502e72f4530ca309a5927554e9
Isomeleze Molosi <molosi@tokkie.onmicrosoft.com>
2022-08-30 08:29:17 +0200

    initial project setup

---
+++ Chelsea.uii/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Chelsea.uii.Startup))]
+namespace Chelsea.uii
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
