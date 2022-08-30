a2bad5d0ce0c4e4c62334e7f84537208d10e6cf4
Isomeleze Molosi <molosi@tokkie.onmicrosoft.com>
2022-08-30 13:12:33 +0200

    Added Product  and Product Repository

---
+++ Chelsea.Data.AccessMemory/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chelsea.core;
+using System.Runtime.Caching;
+using Chelsea.core.Models;
+
+namespace Chelsea.Data.AccessMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+    }
+
+}
+
+    
+ 
