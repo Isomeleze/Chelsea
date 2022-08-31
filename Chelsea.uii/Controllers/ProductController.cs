d6017fa7ac7ff4fdea3283c1c7cd8b381a8a34fc
Isomeleze Molosi <molosi@tokkie.onmicrosoft.com>
2022-08-31 12:25:59 +0200

    Added Product controller

---
+++ Chelsea.uii/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chelsea.core.Models;
+using Chelsea.Data.AccessMemory;
+
+
+namespace Chelsea.uii.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductRepository contex;
+
+        public ProductController()
+        {
+            contex = new ProductRepository();
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = contex.Collection().ToList();
+            return View(products);
+        }
+    }
+}
