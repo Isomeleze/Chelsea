bdd24b69207de60087b7a8fe631cd2c888e5c380
Isomeleze Molosi <molosi@tokkie.onmicrosoft.com>
2022-09-01 16:56:21 +0200

    Finished product controller

--- Chelsea.uii/Controllers/ProductController.cs
+++ Chelsea.uii/Controllers/ProductController.cs
@@ -1,27 +1,115 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Chelsea.core.Models;
 using Chelsea.Data.AccessMemory;
+using Microsoft.Ajax.Utilities;
 
 
 namespace Chelsea.uii.Controllers
 {
     public class ProductController : Controller
     {
-        ProductRepository contex;
+        ProductRepository context;
 
         public ProductController()
         {
-            contex = new ProductRepository();
+            context = new ProductRepository();
         }
         // GET: Product
         public ActionResult Index()
         {
-            List<Product> products = contex.Collection().ToList();
+            List<Product> products = context.Collection().ToList();
             return View(products);
         }
+
+        public ActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+                return RedirectToAction("Index");
+
+            }
+        }
+        public  ActionResult Edit(string Id)
+        {
+            Product product = context.Find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product, string Id)
+        {
+            Product productToEdit = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if(!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+                productToEdit.Catergory = product.Catergory;
+                productToEdit.Description = product.Description;
+                productToEdit.Image = product.Image;
+                productToEdit.Name = product.Name;
+                productToEdit.Price = product.Price;
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Delete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productToDelete);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            Product productToDelete = context.Find(Id);
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete("Index");
+                return RedirectToAction("Index");
+            }
+        }
+        
+            
     }
+       
+
+  
 }
+
+    
