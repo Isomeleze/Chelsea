9685bb8c6aacee01e954fe0753914c7426465e6a
Isomeleze Molosi <molosi@tokkie.onmicrosoft.com>
2022-09-04 12:37:55 +0200

    Finished product controller

---
+++ Chelsea.uii/Controllers/CatergoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chelsea.uii.Controllers
+{
+    public class CatergoryController : Controller
+    {
+        // GET: Catergory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
