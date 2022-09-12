32bcc2214638409a950e71f6a0a35895abf14a1f
Isomeleze Molosi <molosi@tokkie.onmicrosoft.com>
2022-09-12 10:07:46 +0200

    Installed EntityFramework

---
+++ Chelsea.Data.Accesss.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Chelsea.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chelsea.Data.Accesss.SQL
+{
+   public class DataContext : DbContext
+    {
+
+        public DataContext()
+           :base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCatergory> ProductCatergories { get; set; }
+    }
+}
