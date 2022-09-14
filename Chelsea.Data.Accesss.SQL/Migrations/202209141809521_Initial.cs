577c2157e3fb7444ae360ff4a6daf2782e46dd69
Isomeleze Molosi <molosi@tokkie.onmicrosoft.com>
2022-09-14 20:27:57 +0200

    SQL Repository

---
+++ Chelsea.Data.Accesss.SQL/Migrations/202209141809521_Initial.cs
@@ -0,0 +1,40 @@
+﻿namespace Chelsea.Data.Accesss.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductCatergories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Catergory = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 25),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Catergory = c.String(),
+                        Image = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductCatergories");
+        }
+    }
+}
